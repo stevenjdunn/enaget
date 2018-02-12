425c372eae352823d6416908d04ea78ec49895b6
Steven <mail@stevendunn.co.uk>
2018-02-12 11:45:53 +0000

    Added argparse
    Usage:
    -i <path_to_input_file.txt>
    -o <path_to_output_url_list.txt>
    
    Takes list of accession numbers as -i and writes list of wget-able URLs to -o.
    If requested, wget will download files to the directory the EnaGet is invoked from.

--- enaget.py
+++ enaget.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 import subprocess
+import argparse
+
+# argparse setup
+# argparse argument setup
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input", required=True ,help="Path to file containing list of barcodes and their corresponding sample name.")
+parser.add_argument("-o", "--output", required=True, help="Path to output destination")
+parser.add_argument("--man", help="Uses a list of accession numbers to generate and optionally download read data from the ENA. -i <path_to_input_file.txt> -o <path_to_output_url_list.txt>")
+args = parser.parse_args()
 
 # Import Accession No.
-inputfile = raw_input("Path to accession no. text file:")
-with open(inputfile) as f:
+with open(args.input) as f:
   accessions = f.read().splitlines()
   print accessions
 
@@ -18,24 +26,36 @@ prefix_url = ['ftp://ftp.sra.ebi.ac.uk/vol1/' + x[:6] + '/' for x in prefix_no]
 final_url = [x+y for x,y in zip(prefix_url, prefix_no)]
 
 # Write URLs to file
-with open('out.txt','w') as output:
+with open(args.output,'w') as output:
     output.write('\n'.join(final_url))
-
+print ''
+print ''
 print '##############'
 print 'Output written'
 print '##############'
-print ''
+
+
 # Download files
 if download in yes:
-    subprocess.call(['wget','-i','out.txt'])
+    print ''
+    print ''
+    print 'Commencing download...'
+    print ''
+    print ''
+    subprocess.call(['wget','-i', args.output])
 
     print ''
     print '###################'
     print 'Downloads complete'
     print '###################'
 else:
-    print 'URLs written to out.txt in:'
+    print ''
+    print ''
+    print 'URLs written to:',
+    print args.output,
+    print 'in the following directory:'
     subprocess.call('pwd')
+    print ''
     print ''
     print '########'
     print 'Complete'

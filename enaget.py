e35009b786025523fea3c0aa84de970f1445cae8
Steven <mail@stevendunn.co.uk>
2018-02-28 08:35:45 +0000

    Version 0.2
    - Overhauled link generation
        • Previously, ENA would get picky about which accession read data was stored under. I didn't understand the link syntax.
        • Script now pulls a report from ENA which *should* provide accession number cross-compatability for FastQ retrieval.
    - Added option to remove temp files produced by new link generation
    - Altered wget options:
        • Added -t 5 to limited number of retries, preventing endless loop requiring keyboard interrupt.
        • Added -c to avoid duplicate downloads if processing numerous projects with repeat records.

--- enaget.py
+++ enaget.py
@@ -2,16 +2,32 @@
 import subprocess
 import argparse
 import os
+import sys
 
 # Version
-_version_ = "0.1.1"
+_version_ = "0.2"
 
 # argparse argument setup
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", required=True ,help="Path to file containing list of accession numbers.")
 parser.add_argument("-o", "--output", required=True, help="Path to output fastq files.")
 parser.add_argument("-l", "--list", action="store_true", help="Generates a list of URL's for retrieval at a later time (i.e. does not invoke wget).")
+parser.add_argument("-r", "--remove", action="store_true", help="Removes intermediate files (i.e. temp folder in output path)")
 args = parser.parse_args()
+
+# Space Saving
+def scriptend():
+    print ''
+    print ''
+    print ''
+    print ''
+    print 'Author: www.github.com/stevenjdunn'
+    print ''
+    print ''
+    print '################'
+    print 'EnaGet Complete!'
+    print '################'
+    sys.exit(1)
 
 # Directory creation and orientation
 invoked_from = os.getcwd()
@@ -25,20 +41,46 @@ output_path = os.getcwd()
 os.chdir(invoked_from)
 print output_path
 print ''
+temp = output_path + '/temp'
+if not os.path.exists(temp):
+    os.mkdir(temp)
+else:
+    print ''
+    print 'Output directory contains files which will intefere with script and risk being overwritten.'
+    print ''
+    print 'Select a new output directory, or remove the old one.'
+    print ''
+    sys.exit(1)
 
 # Import Accession No.
 with open(args.input) as f:
   accessions = f.read().splitlines()
 
-# Generate URL
+# Generate warehouse report links and write to file
 prefix_no = list(accessions)
-prefix_url = ['ftp://ftp.sra.ebi.ac.uk/vol1/fastq/' + x[:6] + '/' for x in prefix_no]
-final_url = [x+y for x,y in zip(prefix_url, prefix_no)]
+warehouse = ['https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=' + x + '&result=read_run&fields=fastq_ftp&download=txt' for x in prefix_no]
+ENA_warehouse = temp + '/meta_fetch.txt'
+ENA_data = temp + '/ENA_data.csv'
+with open(ENA_warehouse, 'w') as output:
+    output.write('\n'.join(warehouse))
 
-# Write URLs to file
-output_file = output_path + '/url_list.txt'
-with open(output_file,'w') as output:
-    output.write('\n'.join(final_url))
+# Fetch resolved ftp addresses
+print ENA_warehouse
+print ENA_data
+subprocess.call(['wget', '-i', ENA_warehouse, '-O', ENA_data])
+
+# Format CSV for url resolution
+ENA_scrubbed = temp + '/ENA_scrubbed.csv'
+URLs = output_path + '/URL_list.txt'
+with open(ENA_scrubbed, 'w') as csv:
+    for line in file(ENA_data):
+        if 'fastq_ftp' not in line:
+            csv.write(line)
+with open(ENA_scrubbed, 'r') as csv:
+    rawcsv = csv.read()
+rawcsv = rawcsv.replace(';', '\n')
+with open(URLs, 'w') as csv:
+    csv.write(rawcsv)
 print ''
 print ''
 print '##############'
@@ -46,8 +88,21 @@ print 'Output written'
 print '##############'
 print ''
 print 'URLs written to:',
-print output_file
+print URLs
 
+# File cleanup
+if args.remove:
+    import shutil
+    print ''
+    print 'Removing temp files...'
+    print ''
+    shutil.rmtree(temp)
+    print ''
+    print 'Done!'
+
+# --list exit
+if args.list:
+    scriptend()
 
 # Download files
 if not args.list:
@@ -56,19 +111,9 @@ if not args.list:
     print 'Commencing download...'
     print ''
     print ''
-    subprocess.call(['wget', '-m', '-nd', '-i', output_file, '-P', output_path])
-
+    subprocess.call(['wget', '-c', '-t', '5','-nd', '-i', URLs, '-P', output_path])
     print ''
     print '###################'
     print 'Downloads complete'
     print '###################'
-print ''
-print ''
-print ''
-print ''
-print 'Author: www.github.com/stevenjdunn'
-print ''
-print ''
-print '################'
-print 'EnaGet Complete!'
-print '################'
+scriptend()
